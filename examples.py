f39688de33c1cd1475060f666c9826460b87d4a4
Santhosh Kumar Ramakrishnan <sramakri@fb.com>
2020-05-18 13:37:12 -0500

    re-formatted code

--- examples.py
+++ examples.py
@@ -7,29 +7,29 @@ from time import time
 from os.path import basename, join, splitext
 
 # input/output files
-#MAZE_FPATH = join('mazes', 'maze_small.png')
-MAZE_FPATH = join('mazes', 'maze_large.png')
-OUTP_FPATH = join('solns', '%s_soln.png' % splitext(basename(MAZE_FPATH))[0])
+# MAZE_FPATH = join('mazes', 'maze_small.png')
+MAZE_FPATH = join("mazes", "maze_large.png")
+OUTP_FPATH = join("solns", "%s_soln.png" % splitext(basename(MAZE_FPATH))[0])
 
 
 def main():
     maze = cv2.imread(MAZE_FPATH)
     if maze is None:
-        print('no file found: %s' % (MAZE_FPATH))
+        print("no file found: %s" % (MAZE_FPATH))
         return
     else:
-        print('loaded maze of shape %r' % (maze.shape[0:2],))
+        print("loaded maze of shape %r" % (maze.shape[0:2],))
 
     grid = cv2.cvtColor(maze, cv2.COLOR_BGR2GRAY).astype(np.float32)
-    grid[grid == 0] = 1.0#np.inf
+    grid[grid == 0] = 1.0  # np.inf
     grid[grid == 255] = 0.0
 
     # start is the first white block in the top row
-    start_j, = np.where(grid[0, :] == 0)
+    (start_j,) = np.where(grid[0, :] == 0)
     start = np.array([0, start_j[0]])
 
     # end is the first white block in the final column
-    end_i, = np.where(grid[:, -1] == 0)
+    (end_i,) = np.where(grid[:, -1] == 0)
     end = np.array([end_i[0], grid.shape[0] - 1])
 
     t0 = time()
@@ -38,16 +38,16 @@ def main():
     dur = time() - t0
 
     if path.shape[0] > 0:
-        print('found path of length %d in %.6fs' % (path.shape[0], dur))
+        print("found path of length %d in %.6fs" % (path.shape[0], dur))
         maze[path[:, 0], path[:, 1]] = (0, 0, 255)
 
-        print('plotting path to %s' % (OUTP_FPATH))
+        print("plotting path to %s" % (OUTP_FPATH))
         cv2.imwrite(OUTP_FPATH, maze)
     else:
-        print('no path found')
+        print("no path found")
 
-    print('done')
+    print("done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
