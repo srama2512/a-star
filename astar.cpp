e21157a36e2ff28b68dd4129dcb16b7d4911e3f5
Santhosh K. Ramakrishnan <sramakrishnan@utexas.edu>
2021-11-12 03:18:00 -0800

    added multi-goal A* planning

--- astar.cpp
+++ astar.cpp
@@ -247,3 +247,91 @@ extern "C" bool weighted_astar(
 
   return solution_found;
 }
+
+
+// obmap:          flattened h x w grid obstacle map, 1 for obstacle, 0 for free
+// gmap:           flattened h x w grid goal map, 1 for goal, 0 for non-goal
+// h, w:           height and width of grid
+// start:          index of start location in flattened grid
+// goals:          array of indices of a subset of goals from gmap
+// n_goals:        the number of goals
+// diag_ok:        if true, allows diagonal moves (8-conn.)
+// paths (output): for each node, stores previous node in path
+extern "C" int multi_goal_astar(
+      const float* obmap, const float* gmap, const int h, const int w,
+      const int start, const int* goals, const int n_goals, bool diag_ok,
+      int* paths) {
+
+  const float INF = std::numeric_limits<float>::infinity();
+
+  Node start_node(start, 0.);
+
+  float* costs = new float[h * w];
+  for (int i = 0; i < h * w; ++i)
+    costs[i] = INF;
+  costs[start] = 0.;
+
+  std::priority_queue<Node> nodes_to_visit;
+  nodes_to_visit.push(start_node);
+
+  int* nbrs = new int[8];
+  float nbrs_costs [8] = {sqrt(2), 1, sqrt(2), 1, 1, sqrt(2), 1, sqrt(2)};
+
+  int solution_idx = -1;
+  while (!nodes_to_visit.empty()) {
+    // .top() doesn't actually remove the node
+    Node cur = nodes_to_visit.top();
+
+    if (gmap[cur.idx] == 1) {
+      solution_idx = cur.idx;
+      break;
+    }
+
+    nodes_to_visit.pop();
+
+    int row = cur.idx / w;
+    int col = cur.idx % w;
+    // check bounds and find up to eight neighbors: top to bottom, left to right
+    nbrs[0] = (diag_ok && row > 0 && col > 0)          ? cur.idx - w - 1   : -1;
+    nbrs[1] = (row > 0)                                ? cur.idx - w       : -1;
+    nbrs[2] = (diag_ok && row > 0 && col + 1 < w)      ? cur.idx - w + 1   : -1;
+    nbrs[3] = (col > 0)                                ? cur.idx - 1       : -1;
+    nbrs[4] = (col + 1 < w)                            ? cur.idx + 1       : -1;
+    nbrs[5] = (diag_ok && row + 1 < h && col > 0)      ? cur.idx + w - 1   : -1;
+    nbrs[6] = (row + 1 < h)                            ? cur.idx + w       : -1;
+    nbrs[7] = (diag_ok && row + 1 < h && col + 1 < w ) ? cur.idx + w + 1   : -1;
+
+    float heuristic_cost;
+    float curr_heuristic_cost;
+    for (int i = 0; i < 8; ++i) {
+      // if the neighbor is within bounds and is not an obstacle
+      if (nbrs[i] >= 0 && obmap[nbrs[i]] == 0) {
+        // the sum of the cost so far and the cost of this move
+        float new_cost = costs[cur.idx] + nbrs_costs[i];
+        if (new_cost < costs[nbrs[i]]) {
+          // estimate the cost to the goals based on legal moves
+          heuristic_cost = l2_norm(nbrs[i] / w,  nbrs[i] % w,
+                                  goals[0] / w, goals[0] % w);
+          for (int j = 1; j < n_goals; ++j){
+            curr_heuristic_cost = l2_norm(nbrs[i] / w,  nbrs[i] % w,
+                                         goals[j] / w, goals[j] % w);
+            if (curr_heuristic_cost < heuristic_cost){
+              heuristic_cost = curr_heuristic_cost;
+            }
+          }
+          // paths with lower expected cost are explored first
+          float priority = new_cost + heuristic_cost;
+          nodes_to_visit.push(Node(nbrs[i], priority));
+
+          costs[nbrs[i]] = new_cost;
+          paths[nbrs[i]] = cur.idx;
+        }
+      }
+    }
+  }
+
+  delete[] costs;
+  delete[] nbrs;
+
+  return solution_idx;
+}
